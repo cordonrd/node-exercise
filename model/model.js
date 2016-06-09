60f6732fce843bb042d8890aadeb55665b515575
Cordon Davies <cordondavies@Cordons-MacBook-Pro.local>
2016-06-08 20:09:37 -0600

    Characters page done.

--- model/model.js
+++ model/model.js
@@ -4,38 +4,31 @@ var request = Promise.promisify(require("request"));
 exports.getAllCharacters = getAllCharacters;
 
 function getAllCharacters(callback){
-    var options = {
-        url: "http://swapi.co/api/people?format=json",
-        headers: {
-            "content-type": "application/json"
+    var pageLimit = 5; //Each page has 10 results, and characters is supposed to return 50 results.
+    var urls = [];
+    for(var i=1; i <= pageLimit; i++){
+        urls.push("http://swapi.co/api/people?format=json&page="+i);
+    }
+    Promise.map(urls,
+        function(url){
+            var options = {
+                url: url,
+                headers: {
+                    "content-type": "application/json"
+                },
+                json:true
+            };
+            return request(options);
         },
-        json:true
-    };
-    request(options).then(function(data){
-        var totalPages = Math.ceil(parseInt(data.body.count))/10;
-        var urls = [];
-        for(var i=1; i <= totalPages; i++){
-            urls.push("http://swapi.co/api/people?format=json&page="+i);
-        }
-        Promise.map(urls,
-            function(url){
-                var options = {
-                    url: url,
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    json:true
-                };
-                return request(options);
-            },
-            {concurrency:10}
-        ).then(function(list){
-            var characters = list.map(function(item){
-                return item.body.results
-            });
-            callback(characters);
-        })
-
-    }).catch(console.error)
+        {concurrency:10} // so that if there were more than 10 requests at once, there would only be 10 allowed at a time.
+    ).then(function(list){
+        var characters = [];
+        list.forEach(function(page){
+            page.body.results.forEach(function(person) {
+                characters.push(person);
+            })
+        });
+        callback(characters);
+    })
 
 }
