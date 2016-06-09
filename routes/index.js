0ad82112671917432a2fbf8a569cabc3b217225f
Cordon Davies <cordondavies@Cordons-MacBook-Pro.local>
2016-06-09 01:33:56 -0600

    finishing touches

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'SWAPI Node Exercise' });
 });
 router.get('/characters', function(req,res){
   model.getAllCharacters(function(characters){
